652afac37c7da027d354cdebfd6ea28733b4ce48
asauerwein <andreas@sauerwein.se>
2020-11-28 23:03:08 +0100

    added ca model

--- ca-helper/app/models.py
+++ ca-helper/app/models.py
@@ -8,3 +8,14 @@ class Certificate(db.Model):
     private_key = db.Column(db.Text)
     cn = db.Column(db.String(64))
     san = db.Column(db.String(64))
+    valid_from = db.Column(db.DateTime)
+    valid_to = db.Column(db.DateTime)
+    template_id = db.Column(db.Integer, db.ForeignKey("templates.id"))
+    ca_id = db.Column(db.Integer, db.ForeignKey("certificates.id"))
+
+
+class Template(db.Model):
+    __tablename__ = "templates"
+    id = db.Column(db.Integer, primary_key=True)
+    key_usage = db.Column(db.String)
+    certificates = db.relationship('Certificate', backref='template')
